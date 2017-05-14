e8a01827cdbfb47c06f6b917634fec409847b4bc
Charlie-Ng <kcng@ucdavis.edu>
2017-05-14 15:59:50 -0700

    added reset logic

--- src/app/app.tests.service.ts
+++ src/app/app.tests.service.ts
@@ -5,5 +5,14 @@ export class TestsService {
 
   correctAnswerCount: number = 0;
 
+  theoryTestSet: Array<{no: number, question: string, choices: Array<{choice: string, answer: string}>, correctAnswer: string, userAnswer: string}>;
 
+  newTheoryTestSet() {
+    return [
+      {no: 1, question: "This is test1",
+        choices: [{choice: "a", answer: "answer11123123 13123123 123123 123 123 123 12 answer11123123 13123123 123123 123 123 123 12 answer11123123 13123123 123123 123 123 123 12 answer11123123 13123123 123123 123 123 123 12 answer11123123 13123123 123123 123 123 123 12 answer11123123 13123123 123123 123 123 123 12 answer11123123 13123123 123123 123 123 123 12 answer11123123 13123123 123123 123 123 answer11123123 13123123 123123 123 123 123 12answer11123123 13123123 123123 123 123 123 12answer11123123 13123123 123123 123 123 123 12answer11123123 13123123 123123 123 123 123 12answer11123123 13123123 123123 123 123 123 12123 12 answer11123123 13123123 123123 123 123 123 12 answer11123123 13123123 123123 123 123 123 12 "}, {choice: "b", answer: "answer2 123 123 123123 123 12 3"}, {choice: "c", answer: "answer3"}], correctAnswer: "b", userAnswer: ""},
+      {no: 2, question: "This is test2",
+        choices: [{choice: "a", answer: "answer11"}, {choice: "b", answer: "answer22"}, {choice: "c", answer: "answer33"}], correctAnswer: "b", userAnswer: ""}
+    ];
+  }
 }
--- src/pages/tests-theory/tests-theory.ts
+++ src/pages/tests-theory/tests-theory.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController} from 'ionic-angular';
 import { TestsService } from '../../app/app.tests.service';
 
 /**
@@ -18,14 +18,8 @@ export class TestsTheoryPage {
   theoryTestSet: Array<{no: number, question: string, choices: Array<{choice: string, answer: string}>, correctAnswer: string, userAnswer: string}>;
   currentTheoryTest: {no: number, question: string, choices: Array<{choice: string, answer: string}>, correctAnswer: string, userAnswer: string};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private testsService: TestsService) {
-    this.theoryTestSet = [
-      {no: 1, question: "This is test1",
-        choices: [{choice: "a", answer: "answer11123123 13123123 123123 123 123 123 12 answer11123123 13123123 123123 123 123 123 12 answer11123123 13123123 123123 123 123 123 12 answer11123123 13123123 123123 123 123 123 12 answer11123123 13123123 123123 123 123 123 12 answer11123123 13123123 123123 123 123 123 12 answer11123123 13123123 123123 123 123 123 12 answer11123123 13123123 123123 123 123 answer11123123 13123123 123123 123 123 123 12answer11123123 13123123 123123 123 123 123 12answer11123123 13123123 123123 123 123 123 12answer11123123 13123123 123123 123 123 123 12answer11123123 13123123 123123 123 123 123 12123 12 answer11123123 13123123 123123 123 123 123 12 answer11123123 13123123 123123 123 123 123 12 "}, {choice: "b", answer: "answer2 123 123 123123 123 12 3"}, {choice: "c", answer: "answer3"}], correctAnswer: "b", userAnswer: ""},
-      {no: 2, question: "This is test2",
-        choices: [{choice: "a", answer: "answer11"}, {choice: "b", answer: "answer22"}, {choice: "c", answer: "answer33"}], correctAnswer: "b", userAnswer: ""}
-        ];
-
+  constructor(public navCtrl: NavController, public navParams: NavParams, private testsService: TestsService, public alertCtrl: AlertController) {
+    this.theoryTestSet = this.testsService.newTheoryTestSet();
     this.currentTheoryTest = this.theoryTestSet[0];
 
   }
@@ -46,6 +40,29 @@ export class TestsTheoryPage {
     }else {
       // jump to sign test
     }
+  }
+
+  resetTestSet() {
+    let alert = this.alertCtrl.create({
+      title: '重新開始？',
+      enableBackdropDismiss: true,
+      buttons: [
+        {
+          text: '確認',
+          handler: () => {
+            this.theoryTestSet = this.testsService.newTheoryTestSet();
+            this.currentTheoryTest = this.theoryTestSet[0];
+
+          }},
+        {
+          text: '取消',
+          handler: () => {
+
+          }}
+      ]
+    });
+
+    alert.present();
   }
 
 
